7fc89681ee03144fc87782f856a0b16a1337bd8d
Alessandro Ledesma <sandroledesma@gmail.com>
2024-06-03 15:34:50 -0400

    update files

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    num = 1
+    for dwarf in dwarves:
+        print(str(num) + '.', dwarf)
+        # print(f"{num}. {dwarf}")
+        num += 1
+
+def summon_captain_planet(planeteer_calls):
+    result = []
+    for calls in planeteer_calls:
+        new_call = calls.capitalize() + "!"
+        result.append(new_call)
+    return result
 
 def long_planeteer_calls():
     pass
 
 def find_the_cheese():
     pass
+
+roll_call_dwarves(["Dopey", "Grumpy", "Bashful"])
+print(summon_captain_planet(["carrot", "cucumber", "pepper"]))
